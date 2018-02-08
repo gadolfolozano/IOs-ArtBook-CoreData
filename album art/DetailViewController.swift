6f54c37676c174ac1a36f6f24664e21776786832
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2018-02-07 21:32:59 -0500

    save data to coreData

--- album art/DetailViewController.swift
+++ album art/DetailViewController.swift
@@ -7,29 +7,59 @@
 //
 
 import UIKit
+import CoreData
+class DetailViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
-class DetailViewController: UIViewController {
-
+    @IBOutlet weak var mImageView: UIImageView!
+    @IBOutlet weak var mNameTxtField: UITextField!
+    @IBOutlet weak var mCommentTxtField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        mImageView.isUserInteractionEnabled = true
+        let gestureReconizer = UITapGestureRecognizer(target: self, action: #selector(DetailViewController.selectImage))
+        mImageView.addGestureRecognizer(gestureReconizer)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc func selectImage(){
+        print("selectImge")
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        self.present(imagePicker, animated: true, completion: nil)
     }
-    */
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        mImageView.image = chosenImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onSaveClicked(_ sender: Any) {
+        let nameInput = mNameTxtField.text
+        let commentInput = mCommentTxtField.text
+        let image = mImageView.image
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let newArt = NSEntityDescription.insertNewObject(forEntityName: "Pictures", into: context)
+        
+        newArt.setValue(nameInput, forKey: "name")
+        newArt.setValue(commentInput, forKey: "detail")
+        
+        let data = UIImageJPEGRepresentation(image!, 0.5)
+        newArt.setValue(data, forKey: "image")
 
+        do {
+            try context.save()
+            print("Saved!")
+        } catch {
+            print("error")
+        }
+        
+    }
+    
 }
